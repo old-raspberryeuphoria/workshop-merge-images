22efbe28aba7a04b234dfa58abddfdb0c72a8ddf
Thomas Mion <thomas.mion@mistertemp.com>
2020-04-06 09:43:55 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+### Init Project
+
+This will install dependencies: "canvas", "image-size" and "merge-images".
+
+`npm i`
+
+### Merge images
+
+This will merge "images/recto.jpg" and "images/verso.jpg" into "images/output.jpg". Try replacing these images to generate another output!
+
+`npm run start`
---
+++ index.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+const mergeImages = require("merge-images");
+const sizeOf = require("image-size");
+const { Canvas, Image } = require("canvas");
+
+const dimensionsRecto = sizeOf("images/recto.jpg");
+const dimensionsVerso = sizeOf("images/verso.jpg");
+
+const outputFormat = "jpeg";
+
+mergeImages(
+  [
+    { src: "images/recto.jpg", x: 0, y: 0 },
+    { src: "images/verso.jpg", x: 0, y: dimensionsRecto.height },
+  ],
+  {
+    Canvas,
+    Image,
+    format: `image/${outputFormat}`,
+    width: Math.max(dimensionsRecto.width, dimensionsVerso.width),
+    height: dimensionsRecto.height + dimensionsVerso.height,
+  }
+).then((base64) => {
+  const regex = `^data:image\/${outputFormat};base64,`;
+  const base64Data = base64.replace(new RegExp(regex), "");
+
+  fs.writeFile("images/output.jpg", base64Data, { encoding: "base64" }, () => {
+    console.log("File written");
+  });
+});
