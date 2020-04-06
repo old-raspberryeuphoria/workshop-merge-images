fc97da7c8a7ca7528ef2248ebfb666d15eee7b0b
Thomas Mion <thomas.mion@mistertemp.com>
2020-04-06 11:17:39 +0200

    feat: output a .jpeg and .png for every use-case

--- index.js
+++ index.js
@@ -5,51 +5,69 @@ const { Canvas, Image } = require("canvas");
 
 for (let i = 1; i <= 7; i++) {
   fs.readdir(`images/case${i}`, (err, files) => {
-    const [recto, verso] = files.filter(
+    const [rectoFilename, versoFilename] = files.filter(
       (file) => file !== ".DS_Store" && file.split(".")[0] !== "output"
     );
 
-    const dimensionsRecto = sizeOf(`images/case${i}/${recto}`);
-    const dimensionsVerso = sizeOf(`images/case${i}/${verso}`);
+    const dimensionsRecto = sizeOf(`images/case${i}/${rectoFilename}`);
+    const dimensionsVerso = sizeOf(`images/case${i}/${versoFilename}`);
 
-    const outputFormat = i % 2 === 0 ? "jpeg" : "png";
+    const recto = {
+      filename: rectoFilename,
+      height: dimensionsRecto.height,
+      width: dimensionsRecto.width,
+    };
 
-    mergeImages(
-      [
-        { src: `images/case${i}/${recto}`, x: 0, y: 0 },
-        {
-          src: `images/case${i}/${verso}`,
-          x: 0,
-          y: dimensionsRecto.height + 1,
-        },
-      ],
-      {
-        Canvas,
-        Image,
-        format: `image/${outputFormat}`,
-        width: Math.max(dimensionsRecto.width, dimensionsVerso.width),
-        height: dimensionsRecto.height + dimensionsVerso.height,
-        quality: 0.8,
-      }
-    )
-      .then((base64) => {
-        const regex = `^data:image\/${outputFormat};base64,`;
-        const base64Data = base64.replace(new RegExp(regex), "");
+    const verso = {
+      filename: versoFilename,
+      height: dimensionsVerso.height,
+      width: dimensionsVerso.width,
+    };
 
-        fs.writeFile(
-          `images/case${i}/output.${outputFormat}`,
-          base64Data,
-          { encoding: "base64" },
-          () => {
-            console.log(
-              `File written into ./images/case${i}/output.${outputFormat}`
-            );
-          }
-        );
-      })
-      .catch((err) => {
-        console.log(`Err with case ${i}`);
-        console.log(err);
-      });
+    const mergeRectoVersoWithFormat = (outputFormat) =>
+      mergeRectoVerso(`case${i}`, recto, verso, outputFormat);
+
+    mergeRectoVersoWithFormat("jpeg");
+    mergeRectoVersoWithFormat("png");
   });
+}
+
+function mergeRectoVerso(folder, recto, verso, outputFormat) {
+  mergeImages(
+    [
+      { src: `images/${folder}/${recto.filename}`, x: 0, y: 0 },
+      {
+        src: `images/${folder}/${verso.filename}`,
+        x: 0,
+        y: recto.height + 1,
+      },
+    ],
+    {
+      Canvas,
+      Image,
+      format: `image/${outputFormat}`,
+      width: Math.max(recto.width, verso.width),
+      height: recto.height + verso.height,
+      quality: 0.8,
+    }
+  )
+    .then((base64) => {
+      const regex = `^data:image\/${outputFormat};base64,`;
+      const base64Data = base64.replace(new RegExp(regex), "");
+
+      fs.writeFile(
+        `images/${folder}/output.${outputFormat}`,
+        base64Data,
+        { encoding: "base64" },
+        () => {
+          console.log(
+            `File written into ./images/${folder}/output.${outputFormat}`
+          );
+        }
+      );
+    })
+    .catch((err) => {
+      console.log(`Err with case ${i} when creating a ${outputFormat} image.`);
+      console.log(err);
+    });
 }
